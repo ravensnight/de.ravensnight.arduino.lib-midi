5f024e126cf5ace71a877220687984f44eaa5506
ravensnight <ravensnight@hoheisels-welt.de>
2025-05-24 13:05:21 +0200

    added utils 1.0.1

--- include/midi/RolandSysexHdr.h
+++ include/midi/RolandSysexHdr.h
@@ -2,8 +2,7 @@
 #define __ROLAND_SYSEX_HDR_H__
 
 #include <Arduino.h>
-#include <ByteInputStream.h>
-#include <ByteOutputStream.h>
+#include <StreamOperators.h>
 
 namespace MIDI {
 
@@ -13,9 +12,9 @@ namespace MIDI {
         uint8_t cmd;             
         uint8_t addr[3];
 
-        friend ByteInputStream& operator >>(ByteInputStream& is, __RolandSysexHdr& hdr);
-        friend ByteOutputStream& operator <<(ByteOutputStream& os, const __RolandSysexHdr& hdr);
-        
+        friend Stream& operator >>(Stream& is, __RolandSysexHdr& hdr);
+        friend Stream& operator <<(Stream& os, const __RolandSysexHdr& hdr);
+
     } RolandSysexHdr;
 
 
--- src/RolandSysexHdr.cpp
+++ src/RolandSysexHdr.cpp
@@ -4,7 +4,7 @@
 using namespace MIDI;
 using namespace LOGGING;
 
-ByteInputStream& operator >>(ByteInputStream& is, __RolandSysexHdr& hdr) {
+Stream& operator >>(Stream& is, __RolandSysexHdr& hdr) {
     is >> hdr.device;
     is >> hdr.model;
     is >> hdr.cmd;
@@ -13,11 +13,13 @@ ByteInputStream& operator >>(ByteInputStream& is, __RolandSysexHdr& hdr) {
     return is;
 }
 
-ByteOutputStream& operator <<(ByteOutputStream& os, __RolandSysexHdr& hdr) {
+Stream& operator <<(Stream& os, __RolandSysexHdr& hdr) {
     os << hdr.device;
     os << hdr.model;
     os << hdr.cmd;
-    os.writeBytes(hdr.addr, 3);
+    os << hdr.addr[0];
+    os << hdr.addr[1];
+    os << hdr.addr[2];
 
     return os;
 }
